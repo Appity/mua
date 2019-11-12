2fe08d1ea20275140f48e81d96dca28e433b1e9a
Scott Tadman <tadman@postageapp.com>
2019-11-12 17:23:20 -0500

    EmailAddress validator

---
+++ spec/unit/mua_email_address_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Mua::EmailAddress do
+  it 'can validate RFC822 compliant envelope addresses' do
+    expect_mapping(
+      'test@example.com' => true,
+      '@example.com' => false,
+      '@' => false,
+      nil => false,
+      false => false,
+      'true@false' => true,
+      'Example <address@example.com>' => false
+    ) do |addr|
+      Mua::EmailAddress.valid_envelope_address?(addr)
+    end
+  end
+end

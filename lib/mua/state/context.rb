068851a7bef8a4b98ddcc9dbc7ef20497234b097
Scott Tadman <tadman@postageapp.com>
2019-11-15 18:29:56 -0500

    Adding attribute definition tools to Context to make building simple contexts easy

--- lib/mua/state/context.rb
+++ lib/mua/state/context.rb
@@ -14,6 +14,86 @@ class Mua::State::Context
   attr_boolean :terminated
   
   # == Class Methods ========================================================
+
+  def self.with_attributes(*attr_list, **attr_spec)
+    type = Class.new(self)
+
+    attrs = (
+      attr_list.map do |attr_name|
+        type.send(:attr_reader, attr_name)
+        
+        [ attr_name, { 
+          variable: :"@#{attr_name}",
+          default: nil
+        } ]
+      end + attr_spec.map do |attr_name, attr_value|
+        defaults = {
+          variable: :"@#{attr_name}",
+          default: nil
+        }
+
+        case (attr_value)
+        when Hash
+          attr_value = defaults.merge(attr_value)
+        else
+          attr_value, default_value = defaults, attr_value
+          attr_value[:default] = default_value
+        end
+
+        var = attr_value[:variable]
+
+        if (attr_value[:boolean])
+          type.send(:define_method, :"#{attr_name}?") do
+            instance_variable_get(var)
+          end
+
+          type.send(:define_method, :"#{attr_name}=") do |v|
+            instance_variable_set(var, !!v)
+          end
+
+          type.send(:define_method, :"#{attr_name}!") do |&block|
+            return false if (instance_variable_get(var))
+
+            block&.call
+
+            instance_variable_set(var, true)
+          end
+        else
+          if (defaults[:variable] == var)
+            type.send(:attr_reader, attr_name)
+          else
+            type.send(:define_method, attr_name) do
+              instance_variable_get(var)
+            end
+          end
+        end
+
+        [ attr_name, attr_value ]
+      end
+    ).to_h
+
+    type.send(:define_method, :initialize) do |**args|
+      super()
+
+      attrs.each do |name, meta|
+        if (args.key?(name))
+          instance_variable_set(meta[:variable], args[name])
+        else
+          instance_variable_set(
+            meta[:variable],
+            case (default = meta[:default])
+            when Proc
+              default.call
+            else
+              default
+            end
+          )
+        end
+      end
+    end
+
+    type
+  end
   
   # == Instance Methods =====================================================
 
--- spec/unit/mua_state_context_spec.rb
+++ spec/unit/mua_state_context_spec.rb
@@ -59,4 +59,115 @@ RSpec.describe Mua::State::Context do
     expect(transition.target).to eq(:example)
     expect(transition.state).to eq(:finished)
   end
+
+  context 'can quickly define variant contexts using with_attributes' do
+    context_type = Mua::State::Context.with_attributes(
+      :nil_value,
+      fixed_value: true,
+      with_proc: -> { [ ] },
+      with_customization: {
+        variable: :@customized
+      }
+    )
+
+    it 'returns a class that can be instantiated' do
+      expect(context_type).to be_kind_of(Class)
+
+      context = context_type.new
+
+      expect(context).to be_kind_of(context_type)
+
+      expect(context.nil_value).to be(nil)
+      expect(context.fixed_value).to be(true)
+      expect(context.with_proc).to eq([ ])
+      expect(context.with_customization).to be(nil)
+    end
+
+    it 'can have values customized during object construction' do
+      context = context_type.new(
+        nil_value: 'not_nil',
+        fixed_value: nil,
+        with_proc: false,
+        with_customization: '-'
+      )
+
+      expect(context.nil_value).to eq('not_nil')
+      expect(context.fixed_value).to be(nil)
+      expect(context.with_proc).to be(false)
+      expect(context.with_customization).to eq('-')
+    end
+  end
+
+  context 'can define boolean attributes with ?-style interrogators' do
+    context_type = Mua::State::Context.with_attributes(
+      boolean_value: {
+        boolean: true,
+        default: false
+      }
+    )
+
+    it 'properly defaults' do
+      context = context_type.new
+
+      expect(context).to_not be_boolean_value
+    end
+
+    it 'accepts overrides on initialize' do
+      context = context_type.new(boolean_value: true)
+
+      expect(context).to be_boolean_value
+    end
+
+    it 'accepts input and modifications' do
+      context = context_type.new(boolean_value: true)
+
+      context.boolean_value = false
+
+      expect(context).to_not be_boolean_value
+    end
+
+    it 'converts input values to booleans' do
+      context = context_type.new(boolean_value: nil)
+
+      expect(context).to_not be_boolean_value
+
+      context.boolean_value = false
+
+      expect(context).to_not be_boolean_value
+
+      context.boolean_value = 'yes'
+
+      expect(context).to be_boolean_value
+    end
+
+    it 'implements a quick switcher with ! postfix' do
+      context = context_type.new
+
+      expect(context.boolean_value!).to be(true)
+
+      expect(context.boolean_value!).to be(false)
+    end
+
+    it 'executes blocks only if not set' do
+      context = context_type.new
+      executed = 0
+
+      expect(context).to_not be_boolean_value
+      expect(executed).to eq(0)
+
+      context.boolean_value! do
+        executed += 1
+      end
+
+      expect(context).to be_boolean_value
+      expect(executed).to eq(1)
+
+      context.boolean_value! do
+        executed += 1
+      end
+
+      expect(context).to be_boolean_value
+      expect(executed).to eq(1)
+    end
+  end
 end

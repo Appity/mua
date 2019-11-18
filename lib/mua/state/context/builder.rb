6b334190bbe0251256aea44b4efe1636a7a74f73
Scott Tadman <tadman@postageapp.com>
2019-11-18 18:38:20 -0500

    Renaming Mua::State::Context::Helpers -> Mua::State::Context::Builder and shift all of the class building code into module/test

---
+++ lib/mua/state/context/builder.rb
@@ -0,0 +1,116 @@
+module Mua::State::Context::Builder
+  # == Module Methods =======================================================
+
+  def self.class_with_attributes(attr_list, attr_spec)
+    type = Class.new(Mua::State::Context)
+
+    attrs = remap_attrs(attr_list, attr_spec).map do |attr_name, attr_value|
+      var = attr_value[:variable]
+
+      if (attr_value[:boolean])
+        define_boolean_attribute!(type, attr_name, var)
+      elsif (attr_value[:readonly])
+        define_readonly_attribute!(type, attr_name, var)
+      else
+        define_readwrite_attribute!(type, attr_name, var)
+      end
+
+      [ attr_name, attr_value ]
+    end
+
+    define_initialize!(type, attrs)
+
+    type
+  end
+
+  def self.remap_attrs(attr_list, attr_spec, &block)
+    remapped = attr_list.map do |attr_name|
+      [ attr_name, { 
+        variable: :"@#{attr_name}",
+        default: nil
+      } ]
+    end + attr_spec.map do |attr_name, attr_value|
+      defaults = {
+        variable: :"@#{attr_name}",
+        default: nil
+      }
+
+      case (attr_value)
+      when Hash
+        attr_value = defaults.merge(attr_value)
+      else
+        attr_value, default_value = defaults, attr_value
+        attr_value[:default] = default_value
+      end
+
+      [ attr_name, attr_value ]
+    end
+
+    return remapped.to_h unless (block_given?)
+
+    remapped.map(&block).to_h
+  end
+
+  def self.define_boolean_attribute!(type, attr_name, var)
+    type.send(:define_method, :"#{attr_name}?") do
+      instance_variable_get(var)
+    end
+
+    type.send(:define_method, :"#{attr_name}=") do |v|
+      instance_variable_set(var, !!v)
+    end
+
+    type.send(:define_method, :"#{attr_name}!") do |&block|
+      return false if (instance_variable_get(var))
+
+      block&.call
+
+      instance_variable_set(var, true)
+    end
+  end
+
+  def self.define_readonly_attribute!(type, attr_name, var)
+    if ( :"@#{attr_name}" == var)
+      type.send(:attr_reader, attr_name)
+    else
+      type.send(:define_method, attr_name) do
+        instance_variable_get(var)
+      end
+    end
+  end
+
+  def self.define_readwrite_attribute!(type, attr_name, var)
+    if (:"@#{attr_name}" == var)
+      type.send(:attr_accessor, attr_name)
+    else
+      type.send(:define_method, attr_name) do
+        instance_variable_get(var)
+      end
+      type.send(:define_method, :"#{attr_name}=") do |v|
+        instance_variable_set(var, v)
+      end
+    end
+  end
+
+  def self.define_initialize!(type, attrs)
+    type.send(:define_method, :initialize) do |**args|
+      super()
+
+      attrs.each do |name, meta|
+        if (args.key?(name))
+          instance_variable_set(meta[:variable], args[name])
+        else
+          instance_variable_set(
+            meta[:variable],
+            case (default = meta[:default])
+            when Proc
+              default.call
+            else
+              default
+            end
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/mua_state_context_builder_spec.rb
@@ -0,0 +1,111 @@
+RSpec.describe Mua::State::Context::Builder do
+  describe 'remap_attrs()' do
+    it 'normalizes list vs. hash arguments' do
+      normalized = Mua::State::Context::Builder.remap_attrs(
+        [ :example ],
+        {
+          property: { default: true, boolean: true },
+          secondary: { default: 2, variable: :@second }
+        }
+      )
+
+      expect(normalized).to eq(
+        example: { variable: :@example, default: nil },
+        property: { variable: :@property, default: true, boolean: true },
+        secondary: { variable: :@second, default: 2 }
+      )
+    end
+
+    it 'can take a block argument' do
+      normalized = Mua::State::Context::Builder.remap_attrs(
+        [ :example ],
+        {
+          property: { default: true }
+        }
+      ) do |attr_name, attr_value|
+        attr_value[:original] = attr_name
+
+        [ :"#{attr_name}_x", attr_value ]
+      end
+
+      expect(normalized).to eq(
+        example_x: { variable: :@example, default: nil, original: :example },
+        property_x: { variable: :@property, default: true, original: :property },
+      )
+    end
+  end
+
+  context 'class_with_attributes()' do
+    context_type = Mua::State::Context::Builder.class_with_attributes(
+      [ ],
+      boolean_value: {
+        boolean: true,
+        default: false
+      }
+    )
+
+    it 'properly defaults' do
+      context = context_type.new
+
+      expect(context).to_not be_boolean_value
+    end
+
+    it 'accepts overrides on initialize' do
+      context = context_type.new(boolean_value: true)
+
+      expect(context).to be_boolean_value
+    end
+
+    it 'accepts input and modifications' do
+      context = context_type.new(boolean_value: true)
+
+      context.boolean_value = false
+
+      expect(context).to_not be_boolean_value
+    end
+
+    it 'converts input values to booleans' do
+      context = context_type.new(boolean_value: nil)
+
+      expect(context).to_not be_boolean_value
+
+      context.boolean_value = false
+
+      expect(context).to_not be_boolean_value
+
+      context.boolean_value = 'yes'
+
+      expect(context).to be_boolean_value
+    end
+
+    it 'implements a quick switcher with ! postfix' do
+      context = context_type.new
+
+      expect(context.boolean_value!).to be(true)
+
+      expect(context.boolean_value!).to be(false)
+    end
+
+    it 'executes blocks only if not set' do
+      context = context_type.new
+      executed = 0
+
+      expect(context).to_not be_boolean_value
+      expect(executed).to eq(0)
+
+      context.boolean_value! do
+        executed += 1
+      end
+
+      expect(context).to be_boolean_value
+      expect(executed).to eq(1)
+
+      context.boolean_value! do
+        executed += 1
+      end
+
+      expect(context).to be_boolean_value
+      expect(executed).to eq(1)
+    end
+  end
+end

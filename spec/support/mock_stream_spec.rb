12da0967ed91e70971c8459e353e062304a287bd
Scott Tadman <tadman@postageapp.com>
2019-11-22 13:43:39 -0500

    Fixing MockStream test

--- spec/support/mock_stream_spec.rb
+++ spec/support/mock_stream_spec.rb
@@ -22,14 +22,16 @@ RSpec.describe MockStream do
   end
 
   it 'can create a Mua::State::Context and writable IO pair' do
-    context, io = MockStream.context_writable_io
+    Async do
+      context, io = MockStream.context_writable_io
 
-    expect(context).to be_kind_of(Mua::State::Context)
-    expect(context.input).to be_kind_of(Async::IO::Stream)
-    expect(io).to be_kind_of(IO)
+      expect(context).to be_kind_of(Mua::State::Context)
+      expect(context.input).to be_kind_of(Async::IO::Stream)
+      expect(io).to be_kind_of(Async::IO::Stream)
 
-    io.write("example\n")
+      io.puts('example')
 
-    expect(context.input.read_until("\n", chomp: true)).to eq('example')
+      expect(context.input.gets.chomp).to eq('example')
+    end
   end
 end

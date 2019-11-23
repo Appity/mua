78a07ab19bf2df44618f19152c1ca4ce1b9fda89
Scott Tadman <tadman@postageapp.com>
2019-11-23 16:21:25 -0500

    New fields for SMTP::Context tested

--- spec/unit/mua_smtp_client_context_spec.rb
+++ spec/unit/mua_smtp_client_context_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Mua::SMTP::Client::Context do
+RSpec.describe Mua::SMTP::Client::Context, type: :reactor do
   it 'is a Mua::State::Context' do
     expect(Mua::SMTP::Client::Context.ancestors).to include(Mua::State::Context)
   end
@@ -9,6 +9,7 @@ RSpec.describe Mua::SMTP::Client::Context do
     expect(context.username).to be(nil)
     expect(context.password).to be(nil)
     expect(context.remote).to be(nil)
+    expect(context.task).to be(nil)
     expect(context.hostname).to eq('localhost')
     expect(context.protocol).to be(:smtp)
     expect(context).to_not be_auth_support
@@ -17,6 +18,7 @@ RSpec.describe Mua::SMTP::Client::Context do
     expect(context).to_not be_tls_supported
     expect(context).to_not be_proxy
     expect(context.timeout).to be(Mua::Constants::TIMEOUT_DEFAULT)
+    expect(context.delivery_queue).to eq([ ])
   end
 
   it 'allows writing to properties' do
@@ -25,6 +27,7 @@ RSpec.describe Mua::SMTP::Client::Context do
     context.username = 'user'
     context.password = 'pass'
     context.remote = 'mail.example.net'
+    context.task = reactor
     context.hostname = 'mta.example.org'
     context.protocol = :esmtp
     context.auth_support!
@@ -33,10 +36,16 @@ RSpec.describe Mua::SMTP::Client::Context do
     context.tls_supported!
     context.proxy!
     context.timeout = 999
+    context.delivery_queue << {
+      mail_from: 'mail-from@example.org',
+      rcpt_to: 'rcpt-to@example.org',
+      data: 'From: Demo'
+    }
 
     expect(context.username).to eq('user')
     expect(context.password).to eq('pass')
     expect(context.remote).to eq('mail.example.net')
+    expect(context.task).to be(reactor)
     expect(context.hostname).to eq('mta.example.org')
     expect(context.protocol).to be(:esmtp)
     expect(context).to be_auth_support
@@ -45,5 +54,10 @@ RSpec.describe Mua::SMTP::Client::Context do
     expect(context).to be_tls_supported
     expect(context).to be_proxy
     expect(context.timeout).to eq(999)
+    expect(context.delivery_queue).to eq([{
+      mail_from: 'mail-from@example.org',
+      rcpt_to: 'rcpt-to@example.org',
+      data: 'From: Demo'
+    }])
   end
 end

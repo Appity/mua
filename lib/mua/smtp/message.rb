2ae59329152c4eb67aa5d9f670078ba0a9e78881
Scott Tadman <tadman@postageapp.com>
2019-11-24 18:13:00 -0500

    Adding new fields to SMTP::Message to capture results and feed back data.

--- lib/mua/smtp/message.rb
+++ lib/mua/smtp/message.rb
@@ -1,28 +1,60 @@
 class Mua::SMTP::Message
   # == Constants ============================================================
+
+  STATES = %i[
+    queued
+    delivered
+    rejected
+    bounced
+    failed
+  ].freeze
+
+  STATE_DEFAULT = :queued
   
   # == Extensions ===========================================================
   
   # == Properties ===========================================================
 
+  attr_reader :id
   attr_reader :mail_from
   attr_reader :rcpt_to
   attr_reader :data
+
+  attr_accessor :state
+  attr_reader :reply_code
+  attr_accessor :reply_message
   
   # == Class Methods ========================================================
+
+  def self.states
+    STATES
+  end
   
   # == Instance Methods =====================================================
   
   def initialize(args = nil)
     args ||= { }
     
+    @id = args[:id] || args['id']
     @mail_from = args[:mail_from] || args['mail_from']
     @rcpt_to = args[:rcpt_to] || args['rcpt_to']
     @data = (args[:data] || args['data'])&.to_s&.gsub(/\r?\n/, "\r\n")
     @test = !!(args[:test] || args['test'])
+
+    @state = (args[:state] || args['state'] || STATE_DEFAULT).to_sym
   end
 
   def test?
     @test
+  end
+
+  STATES.each do |s|
+    define_method(:"#{s}?") do
+      @state == s
+    end
+  end
+
+  def reply_code=(v)
+    @reply_code = v&.to_i
   end
 end
--- spec/unit/mua_smtp_message_spec.rb
+++ spec/unit/mua_smtp_message_spec.rb
@@ -6,20 +6,30 @@ RSpec.describe Mua::SMTP::Message do
     expect(message.rcpt_to).to be(nil)
     expect(message.data).to be(nil)
     expect(message).to_not be_test
+
+    expect(message).to be_queued
+    expect(message.state).to eq(:queued)
+    expect(message.reply_code).to be(nil)
+    expect(message.reply_message).to be(nil)
   end
 
   it 'takes Symbol-keyed arguments' do
     message = Mua::SMTP::Message.new(
+      id: 'fea199da483c@mta.example.org',
       mail_from: 'mail-from@example.org',
       rcpt_to: 'rcpt-to@example.org',
       data: 'Demo: true',
-      test: 'very yes'
+      test: 'very yes',
+      state: 'failed'
     )
 
+    expect(message.id).to eq('fea199da483c@mta.example.org')
     expect(message.mail_from).to eq('mail-from@example.org')
     expect(message.rcpt_to).to eq('rcpt-to@example.org')
     expect(message.data).to eq('Demo: true')
     expect(message).to be_test
+    expect(message).to be_failed
+    expect(message.state).to eq(:failed)
   end
 
   it 'takes String-keyed arguments' do
@@ -27,12 +37,38 @@ RSpec.describe Mua::SMTP::Message do
       'mail_from' => 'mail-from@example.org',
       'rcpt_to' => 'rcpt-to@example.org',
       'data' => 'Demo: true',
-      'test' => 'very yes'
+      'test' => 'very yes',
+      'state' => 'failed'
     )
 
     expect(message.mail_from).to eq('mail-from@example.org')
     expect(message.rcpt_to).to eq('rcpt-to@example.org')
     expect(message.data).to eq('Demo: true')
     expect(message).to be_test
+  end
+
+  it 'can be set to different states' do
+    message = Mua::SMTP::Message.new
+
+    states = Mua::SMTP::Message.states
+
+    states.each do |state|
+      message.state = state
+
+      # Only one state is true, the rest are false
+      states.each do |s|
+        expect(message.send(:"#{s}?")).to eq(s == state)
+      end
+    end
+  end
+  
+  it 'can be updated with a reply code/message' do
+    message = Mua::SMTP::Message.new
+
+    message.reply_code = '250'
+    message.reply_message = 'We got it'
+
+    expect(message.reply_code).to eq(250)
+    expect(message.reply_message).to eq('We got it')
   end
 end
